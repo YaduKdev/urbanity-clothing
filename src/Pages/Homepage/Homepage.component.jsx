709185bca358b9b5bab4f2ede6c5278f84d8b658
YaduKdev <yaduk.developer@gmail.com>
2021-08-09 17:30:44 +0530

    practised styled components on homepage

--- src/Pages/Homepage/Homepage.component.jsx
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import DirectoryMenu from '../../Components/DirectoryMenu/DirectoryMenu.component';
-import './Homepage.styles.scss';
+import { Banner, BannerContainer, Filler } from './Homepage.styles';
 
 class Homepage extends React.Component {
     render () {
         return (
             <div className='homepage'>
-                <div className='banner-container'>
-                <img src={process.env.PUBLIC_URL+'/Images/banner.jpg'} alt='banner' className='banner' />
-                </div>
+                <BannerContainer>
+                <Banner src={process.env.PUBLIC_URL+'/Images/banner.jpg'} alt='banner' />
+                </BannerContainer>
                 <div className='directory-menu-container'>
                     <DirectoryMenu />
                 </div>
-                <div className='filler' />
+                <Filler />
             </div>
         )
     }
---
+++ src/Pages/Homepage/Homepage.styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const Banner = styled.img`
+    width: 100%;
+    height: 30%;
+    box-shadow: 2px 2px 4px rgb(0 0 0 / 0.2);
+`
+
+export const BannerContainer = styled.div`
+    margin-bottom: 150px;
+`
+
+
+export const Filler = styled.div`
+    position: absolute;
+    top: 2300px;
+    height: 150px;
+    width: 100%;
+`
